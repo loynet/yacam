9107c920a0898e6360171be5782012c90de2baf7
loynet <loynet@protonmail.com>
2023-09-29 18:49:02 +0100

    Refactor the listener and update README.md

---
+++ yacam/eval.py
@@ -0,0 +1,85 @@
+from abc import ABC, abstractmethod
+from configparser import ConfigParser
+from urlextract import URLExtract
+
+from post import Post
+
+import re
+
+
+# Ideally this would be an interface, but python isn't go so too bad :(
+class StringEval(ABC):
+    @abstractmethod
+    def is_spam(self, s: str) -> bool:
+        pass
+
+
+class Threshold(StringEval):
+    def __init__(self, config: ConfigParser):
+        tokens = "[" + "".join([re.escape(e) for e in config['detection']['tokens'].split()]) + "]"
+        self.pattern = re.compile(tokens, re.IGNORECASE)
+        self.max = config['detection'].getfloat('max_threshold')
+
+    def is_spam(self, s: str) -> bool:
+        return len(s) > 0 and (len(re.findall(self.pattern, s)) / len(s)) > self.max
+
+
+class Counter(StringEval):
+    def __init__(self, config: ConfigParser):
+        tokens = "[" + "".join([re.escape(e) for e in config['detection']['tokens'].split()]) + "]"
+        # Compiles the regex for the entry and group of entries
+        e = r'[^\W_]' + tokens
+        self.entry_pattern = re.compile(e, re.IGNORECASE)
+        self.pattern = re.compile(f'(?:{e})+', re.IGNORECASE)
+
+        self.max = config['detection'].getint('max_consecutive_entries')
+
+    def is_spam(self, s: str) -> bool:
+        for found in re.finditer(self.pattern, s):
+            if len(re.findall(self.entry_pattern, found.string[found.start():found.end()])) > self.max:
+                return True
+        return False
+
+
+class PostEval:
+    def __init__(self, config: ConfigParser):
+        mode = config['detection']['mode']
+        if mode == 'threshold':
+            self.str_eval: StringEval = Threshold(config)
+        elif mode == 'entries':
+            self.str_eval: StringEval = Counter(config)
+        else:
+            raise ValueError('detection mode must be "threshold" or "entries"')
+
+        # Read the whitelist configs, it is optional
+        self.whitelist: list[str] = config.get('detection', 'countries_whitelist', fallback='').split()
+
+        self.url_extr: URLExtract = URLExtract()
+
+    def is_spam(self, p: Post) -> bool:
+        # Check if the poster used a capcode, basically if is an admin, mod or part of the global staff
+        if p.has_capcode():
+            return False
+
+        # Check if a post has a messages and files
+        if not p.message or not p.has_files():
+            return False
+
+        # Check if the post is from a whitelisted country
+        if p.has_geo_flag() and p.author.flag.code in self.whitelist:
+            return False
+
+        # 99% of the posts are usually considered not spam at this point
+
+        # Check for urls
+        msg = p.message
+        urls = self.url_extr.find_urls(msg, get_indices=True)
+        # No urls = no problem
+        if len(urls) == 0:
+            return False
+
+        # Remove found urls
+        for url in urls:
+            msg = msg[:url[1][0]] + msg[url[1][1]:]
+
+        return self.str_eval.is_spam(msg)
--- yacam/listener.py
+++ yacam/listener.py
@@ -1,146 +1,36 @@
-import configparser
-import json
+import atexit
 import logging
-import re
-from abc import ABC
-from threading import Thread, Event
 
-import socketio
-from urlextract import URLExtract
-
-from post import Post
 from session import ModSession
+import socketio
 
 logger = logging.getLogger(__name__)
 
 
-# TODO this stopple thread thing is even doing something? Check it later
-class StoppableThread(ABC, Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.daemon = True
-        self._stop = Event()
-
-    def stop(self):
-        logger.debug(f'Killing thread, goodbye')
-        self._stop.set()
-
-
-class PostsListener(StoppableThread):
-    def __init__(self, session: ModSession, config_parser: configparser.ConfigParser) -> None:
-        super(PostsListener, self).__init__()
-
-        # Read the detection configs
-        tokens = "[" + "".join([re.escape(e) for e in config_parser['detection']['tokens'].split()]) + "]"
-        mode = config_parser['detection']['mode']
-
-        if mode == 'threshold':
-            self.tokens_pattern = re.compile(tokens, re.IGNORECASE)
-            self.max_threshold = config_parser['detection'].getfloat('max_threshold')
-            self.eval_post = self.eval_threshold
-
-        elif mode == 'entries':
-            # Compiles the regex for the entry and group of entries
-            e = r'[^\W_]' + tokens
-            self.entry_pattern = re.compile(e, re.IGNORECASE)
-            self.entries_pattern = re.compile(f'(?:{e})+', re.IGNORECASE)
-
-            self.max_entries = config_parser['detection'].getint('max_consecutive_entries')
-            self.eval_post = self.eval_entries
-
-        # Read the whitelist configs
-        self.countries_whitelist = config_parser.get('detection', 'countries_whitelist', fallback='').split()
-
-        # Read the moderation action configs
-        log_message = config_parser.get('moderation', 'log_message', fallback='')
-        self.ban_author = config_parser['moderation']['action'] == "ban"
-        if not self.ban_author:
-            self.mod_action = lambda post: self.session.delete_post(post.board, post.post_id, log_message=log_message)
-        else:
-            duration = config_parser.get('moderation', 'ban_duration', fallback='1y')
-            reason = config_parser.get('moderation', 'ban_reason', fallback='')
-            self.mod_action = lambda post: self.session.delete_ban(post.board, post.post_id, ban_reason=reason,
-                                                                   ban_duration=duration, log_message=log_message)
-
+class PostListener:
+    def __init__(self, session: ModSession, callbacks: list[callable(dict)]):
         self.session = session
-        self.extractor = URLExtract()
+        self.callbacks = callbacks
 
-        client = socketio.Client(http_session=self.session)
+        self.io = socketio.Client(http_session=session)
 
-        @client.event
+        @self.io.event
         def connect():
-            logger.info(f'Connected to {self.session.domain} websocket')
-            client.emit('room', 'globalmanage-recent-hashed')
+            self.io.emit('room', 'globalmanage-recent-hashed')
 
-        @client.event
-        def disconnect():
-            logger.info(f'Disconnected from {self.session.domain} websocket')
+        @self.io.on('newPost')
+        def new_post(data):
+            for c in self.callbacks:
+                c(data)
 
-        @client.on('newPost')
-        def on_new_post(data):
-            if self.handle_new_post(Post.from_raw(data)):
-                # TODO: this is a very "hackish" way to do this, but it works for now, fix it later
-                # Redact urls from the message
-                urls = self.extractor.find_urls(data['nomarkup'])
-                for url in urls:
-                    u = url.split('/')
-                    # If the url has no path, we must delete the whole domain (in every entry)
-                    safe_url = f'{u[0]}/REDACTED' if len(u) > 1 else f'REDACTED.TLD'
-                    data['nomarkup'] = data['nomarkup'].replace(url, safe_url)
-                    data['message'] = data['message'].replace(url, safe_url)
-
-                # Dump the data to a json file
-                with open(f'{data["_id"]}.json', 'w') as f:
-                    f.write(json.dumps(data))
-
-        self.client = client
-        self.run()
-
-    def eval_threshold(self, message: str) -> bool:
-        return len(message) > 0 and len(re.findall(self.tokens_pattern, message)) / len(message) > self.max_threshold
-
-    def eval_entries(self, message: str) -> bool:
-        for found in re.finditer(self.entries_pattern, message):
-            if len(re.findall(self.entry_pattern, found.string[found.start():found.end()])) > self.max_entries:
-                return True
-        return False
-
-    def handle_bad_post(self, post: Post) -> None:
-        logger.info(
-            f'Found bad post:\n{post.get_url()}\n{" ,".join(file.content_hash for file in post.files)}\n{post.message}')
-        self.mod_action(post)
-
-    def handle_new_post(self, post: Post) -> bool:
-        logger.debug(f'New post: {post.get_url()}, {post.message}')
-        # Whitelist posts without files, posts from authenticated authors and posts from safe countries
-        if not post.has_files() or post.has_capcode() or \
-                (post.has_geo_flag() and post.author.flag.code in self.countries_whitelist):
-            return False
-
-        msg = post.message
-        # No message = no problem
-        if msg is None:
-            return False
-
-        # Check for urls
-        urls = self.extractor.find_urls(msg, get_indices=True)
-        # No urls = no problem
-        if len(urls) == 0:
-            return False
-
-        # Remove detected urls
-        for url in urls:
-            msg = msg[:url[1][0]] + msg[url[1][1]:]
-
-        # Calculate the obfuscating ratio and compare it to the threshold
-        if self.eval_post(msg):
-            self.handle_bad_post(post)
-            return True
+        # Disconnect at exit
+        atexit.register(lambda: (
+            self.io.disconnect(),
+            logger.info("Disconnected")
+        ))
 
     def run(self):
-        self.client.connect(f'wss://{self.session.domain}/', transports=['websocket'])
-        self.client.wait()  # Blocks the thread until the connection is closed
-
-        if self._stop.wait():
-            logger.info("Exiting recent watcher")
-            self.client.disconnect()
+        self.io.connect(f'wss://{self.session.domain}/', transports=['websocket'])
+        logger.info("Connected")
+        # Blocks until the connection is stopped
+        self.io.wait()
